d727e43b742731de406ba1be28bb1b8d448aac63
Erdem Bircan <8729400+erdembircan@users.noreply.github.com>
2026-08-17 02:56:52 +0300

    ast 1.3.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ast.rb
+++ Formula/ast.rb
@@ -1,8 +1,8 @@
 class Ast < Formula
   desc "CLI tool that tracks AI model performance (Claude, OpenAI)"
   homepage "https://erdembircan.github.io/ai-stupidity-tracker/"
-  url "https://github.com/erdembircan/ai-stupidity-tracker/archive/refs/tags/v1.3.2.tar.gz"
-  sha256 "ebcfee5ff8a4f0a091792be1867610493c2cdfc6508a6e830a0281bec367ac91"
+  url "https://github.com/erdembircan/ai-stupidity-tracker/archive/refs/tags/v1.3.3.tar.gz"
+  sha256 "2dce943627891651c5be115f67804ef44e5ff3b7ea5415159ac8459178d5f380"
   license "Apache-2.0"
   head "https://github.com/erdembircan/ai-stupidity-tracker.git", branch: "master"
 
